bf42a7f36d3584bdb72a7d543dc4b6153a4d7aa5
Waseem Ashraf <waseemashraf201@gmail.com>
2024-09-10 08:20:45 +0500

    fun, optional para,named ,positional,nested func

--- func.dart
+++ func.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 void main() {
   // function is reusable by passing different value to function
 // what is parameter
@@ -22,4 +24,45 @@ void main() {
   var val = Result();
   // This expression has a type of 'void' so its value can't be used
   // print(val);
+  // anonymous function
+  // dart gives you the possibility to create nameless functions are called anonymous function
+  //example
+  // single line
+  bool Function(int) isEven = (int value) => value % 2 == 0;
+  print(isEven(18));
+  // multi lines
+  final anon = (String nickname) {
+    var name = 'waseem ashraf ';
+    name += nickname;
+    return name;
+  };
+  print(anon('usman ahmad'));
+
+  void test(void Function(int) action) {
+    final items = [1, 2, 3, 4, 5, 6];
+    for (var item in items) {
+      action(item);
+    }
+  }
+
+  test((int value) {
+    print(value);
+  });
+
+  // 3.8 OPTIONAL PARAMETERS
+  //  -3.8.1 Named parameters
+  void testOptional({int? a, int? b}) {
+    print("a is $a");
+    print("b is $b");
+  }
+
+  testOptional(a: 3, b: 9);
+
+  void nested(int value) {
+    int randomNumber() => Random().nextInt(10);
+    final number = value + randomNumber();
+    print("Number is $number");
+  }
+
+  nested(10);
 }
